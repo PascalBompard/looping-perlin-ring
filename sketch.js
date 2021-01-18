4f1a2e6f99f0b8b3c8bfd31b0a93e4df3408e7d4
PascalBompard <hello@pascalbompard.com>
2021-01-18 16:55:50 +0800

    Add files via upload
    adjusting size for mobile viewing, tweaking animations and colours

--- sketch.js
+++ sketch.js
@@ -6,9 +6,16 @@ let shapeCol;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  baseRadius = windowHeight/(6.89);
+  if (windowWidth > windowHeight) {
+    baseRadius = windowHeight/(10);
+  } else {
+    baseRadius = windowWidth/(10);
+  }
+  
   bgCol = color(240,240,230);
-  shapeCol = color('rgba(147, 220, 204, .7)');
+  shapeCol = color('rgba(100, 220, 244, .8)');
+  strokeCol = color(80, 180, 204);
+  strokeCol1 = color('rgba(245, 150, 250, .4)');
 }
 
 function draw() {
@@ -21,10 +28,13 @@ function draw() {
   // fill(255,100);
 
 
-  stroke(0,9);
-  strokeWeight(50);
+  // stroke(0,9);
+  // strokeWeight(5);
+  push();
+    fill(shapeCol)
+ 
   beginShape();
-  for (let a = 0; a < TWO_PI; a+=0.4) {
+  for (let a = 0; a < TWO_PI; a+=0.04) {
     let xoff = map(cos(a), -1, 1, 0, noiseMax-3);
     let yoff = map(sin(a), -1, 1, 0, noiseMax+1);
     let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius+70, baseRadius + 110);
@@ -33,24 +43,25 @@ function draw() {
     vertex(x,y);
   }
   endShape(CLOSE);
-  
-  stroke(0,10);
-  strokeWeight(40);
+  pop();
+
+  stroke(strokeCol1);
+  strokeWeight(2);
   beginShape();
-  for (let a = 0; a < TWO_PI; a+=0.3) {
+  for (let a = 0; a < TWO_PI; a+=0.06) {
     let xoff = map(cos(a), -1, 1, 0, noiseMax-9.5);
     let yoff = map(sin(a), -1, 1, 0, noiseMax+10);
-    let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius+70, baseRadius + 110);
+    let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius+80, baseRadius + 120);
     let x = r * cos(a);
     let y = r * sin(a);
     vertex(x,y);
   }
   endShape(CLOSE);
 
-  stroke(0,13);
-  strokeWeight(20);
+  stroke(strokeCol1);
+  strokeWeight(3);
   beginShape();
-  for (let a = 0; a < TWO_PI; a+=0.2) {
+  for (let a = 0; a < TWO_PI; a+=0.08) {
     let xoff = map(cos(a), -1, 1, 0, noiseMax+2);
     let yoff = map(sin(a), -1, 1, 0, noiseMax+2);
     let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius+70, baseRadius + 110);
@@ -60,10 +71,10 @@ function draw() {
   }
   endShape(CLOSE);
 
-  stroke(0,15);
-  strokeWeight(10);
+  stroke(strokeCol1);
+  strokeWeight(2);
   beginShape();
-  for (let a = 0; a < TWO_PI; a+=0.1) {
+  for (let a = 0; a < TWO_PI; a+=0.05) {
     let xoff = map(cos(a), -1, 1, 0, noiseMax-8);
     let yoff = map(sin(a), -1, 1, 0, noiseMax-8);
     let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius+65, baseRadius + 110);
@@ -73,13 +84,13 @@ function draw() {
   }
   endShape(CLOSE);
   
-  stroke(0,30);
-  strokeWeight(2);
+  stroke(strokeCol);
+  strokeWeight(1);
   beginShape();
-  for (let a = 0; a < TWO_PI; a+=0.05) {
+  for (let a = 0; a < TWO_PI; a+=0.003) {
     let xoff = map(cos(a), -1, 1, 0, noiseMax);
     let yoff = map(sin(a), -1, 1, 0, noiseMax);
-    let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius+75, baseRadius+105);
+    let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius+65, baseRadius+95);
     let x = r * cos(a);
     let y = r * sin(a);
     vertex(x,y);
