19611a9e71c9776b32149f5b918d9a960bb946b6
PascalBompard <hello@pascalbompard.com>
2021-02-02 22:16:56 +0800

    Add files via upload
    iterated HUD to a title plate

--- libraries/hud.js
+++ libraries/hud.js
@@ -1,14 +1,32 @@
 // adds text readout on the screen
 
-function hud(txt){
+function hud(titleTxt, codeTxt, logTxt){
+  let gutter = width/24;
+  let hudWidth = width - (gutter*2);
+  let hudHeight = height/24;
+  let hudTxtSize = gutter/2;
+  let hudHeightAdjust = gutter/4;
+  let titleTxtInset = gutter+10;
+  let logTxtInset = width - gutter - hudWidth/5;
+  let codeTxtInset = logTxtInset - gutter - hudWidth/5 ;
+  
     push();
       // rectMode(CORNER);
-      textAlign (CENTER,BASELINE);
-      textSize(11);
-      noStroke();
-      fill(240);
-      rect(20, height-100, 200, 50)
+      textAlign (LEFT,BASELINE);
+      textSize(hudTxtSize);
+      stroke(50);
+      strokeWeight(1);
+      noFill();
+      // fill(240);
+      rect(gutter, height-(hudHeight + gutter + hudHeightAdjust), hudWidth, hudHeight)
       fill(50);
-      text(txt, 120, height -70 );
+      line(codeTxtInset-10, height-(hudHeight + gutter + hudHeightAdjust),codeTxtInset-10,height-(gutter + hudHeightAdjust));
+      line(logTxtInset-10, height-(hudHeight + gutter + hudHeightAdjust), logTxtInset-10, height-(gutter + hudHeightAdjust));
+      noStroke();
+      
+      text(codeTxt, codeTxtInset, height-((hudHeight + hudHeightAdjust)) );
+      text(logTxt, logTxtInset, height-((hudHeight + hudHeightAdjust)) );
+      textSize(hudTxtSize*1.4);
+      text(titleTxt, titleTxtInset, height-((hudHeight + hudHeightAdjust/2)) );
     pop() ;
 }
--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ let bgCol;
 let shapeCol;
 let numOrbs = 4;
 let fr;
+let title;
+let code;
 
 
 function setup() {
@@ -12,7 +14,7 @@ function setup() {
   if (windowWidth > windowHeight) {
     baseRadius = windowHeight/(numOrbs);
   } else {
-    baseRadius = windowWidth/(numOrbs*numOrbs);
+    baseRadius = windowWidth/(numOrbs);
   }
   
   bgCol = color(245,240,30);
@@ -20,19 +22,21 @@ function setup() {
 
 function draw() {
   background(bgCol);
-  fr = "Frame Rate: " + round(frameRate());
-  hud(fr);
+  fr = "Frame Rate " + round(frameRate());
+  title = "Pulse";
+  code = "0001"
+  hud(title,code,fr);
   translate(width/2, height/2);
   
   for (let i = 1; i < numOrbs; i++) {
-    strokeWeight(20 - (5*i));
+    strokeWeight(30 - (8*i));
     stroke(0,255);
     noFill()   
     beginShape();
     for (let a = 0; a < TWO_PI; a+=(0.008)) {
       let xoff = map(cos(a), -1, 1, 0, noiseMax);
       let yoff = map(sin(a), -1, 1, 0, noiseMax);
-      let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius-(i*20), baseRadius+(i*150));
+      let r = map(noise(xoff, yoff,zoff), 0, 1, baseRadius-(i*(baseRadius/1.2)), baseRadius+(i*(baseRadius)));
       let x = r * cos(a);
       let y = r * sin(a);
       vertex(x,y);
